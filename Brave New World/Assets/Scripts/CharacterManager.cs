dd4b98041f2949bf2d8e887abcda4044212421ab
Justin Mitchell <justin.97.jm@gmail.com>
2018-04-14 21:41:22 -0400

    Game as of 4/14/2018 9:21 PM
    - Path is finished (Random move speeds will be implemented)
    - Audio is back to where it was before the merge problem
    - Spawner also set up
    - Set up the prefabs folder for further objects

--- Brave New World/Assets/Scripts/CharacterManager.cs
+++ Brave New World/Assets/Scripts/CharacterManager.cs
@@ -2,59 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterManager : MonoBehaviour {
-	[SerializeField] private Transform trans;
-	[SerializeField] private Animator anim;
-	//static Animator anim;
+public class CharacterManager : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
 
-	private static float radiusOfSatisfaction = 0.55f;
-	private static float turnSpeed = 10f;
+    GameObject pathGO;
+    Transform targetPathNode;
+    int pathNodeIndex = 0;
 
-	private Vector3 endPoint;
+    private static float radiusOfSatisfaction = 0.75f;
+    private static float turnSpeed = 10f;
+    private static float moveSpeed = 3f;
 
-	public Vector3 destination;
+    // Use this for initialization
+    void Start()
+    {
+        pathGO = GameObject.Find("Path");
+        anim.SetTrigger("idle");
+    }
 
-	// Use this for initialization
-	void Start () {
-		//anim = GetComponent<Animator>();
-		anim.SetTrigger("idle");
-		endPoint = GameObject.FindWithTag("Finish").transform.position;
-		SetDestination (endPoint);
-	}
+    void GetNextPathNode()
+    {
+        targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
+        pathNodeIndex++;
+    }
 
-	// Update is called once per frame
-	void Update () {
-		movement ();
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+        if (targetPathNode == null)
+        {
+            GetNextPathNode();
+            if (targetPathNode == null)
+            {
+                //Ran out of path to follow
+                ReachedGoal();
+            }
+        }
+        Movement();
+    }
 
-	void movement() {
-		float distToDestination = Vector3.Distance (trans.position, endPoint);
+    void Movement()
+    {
 
-		if (distToDestination > radiusOfSatisfaction) {
-			anim.SetTrigger ("isWalking");
-			SetDestination (endPoint);
-		}
+        float distToDestination = Vector3.Distance(transform.position, targetPathNode.position);
+        Vector3 dir = targetPathNode.position - this.transform.position;   //Vector facing the direction towards the pathNode
+        float distThisFrame = moveSpeed * Time.deltaTime;                       //distance that will be covered this frame
+        dir.y = 0;
 
-		if (distToDestination < radiusOfSatisfaction) {
-			anim.SetTrigger ("idle");
-			Destroy (GameObject.FindWithTag("Player"));
-		}
-		Debug.DrawLine (trans.position + Vector3.up, destination, Color.blue);
+        //Animation if's
+        if (distToDestination > radiusOfSatisfaction)
+        {   //Far away enough to keep walking
+            anim.SetTrigger("isWalking");
+        }
+
+        Debug.DrawLine(transform.position + Vector3.up, targetPathNode.position, Color.blue); //Blue is where the bots need to go, the node destination
+       // Debug.DrawLine(transform.position + Vector3.up, dir, Color.red);                       //Red is where they want to go, the "dir" vector
+        Debug.DrawRay(transform.position + Vector3.up, dir, Color.red);
 
 
-		// Face Destination
-		Vector3 towards = destination - trans.position;
-		towards.y = 0f;
-		Quaternion targetRotation = Quaternion.LookRotation (towards);
-		trans.rotation = Quaternion.Lerp (trans.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        //Script if's
+        if (distToDestination < radiusOfSatisfaction)
+        //if (dir.magnitude <= distThisFrame)
+        {               //If length of vector facing destination is shorter than distance covered this frame
+            //Reached node
+            targetPathNode = null;
+        }
+        else
+        {                                            //Can still move this frame without reaching destination
 
-		// Keep grounded
-		if (trans.position.y != -23.97828f)
-			trans.position = new Vector3(trans.position.x, -23.97828f, trans.position.z);
-	}
+            // Move towards destination
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+            Debug.Log("TargetPathNodePosition: " + targetPathNode.position + ", Destination: " + dir);  //compares the node destination to the calculated destination
 
-	public void SetDestination (Vector3 dest) {
-		destination = dest;
-	}
+            //Rotate towards destination
+            Quaternion targetRotation = Quaternion.LookRotation(dir.normalized);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+
+
+            // Keep grounded
+            if (transform.position.y != -23.97828f)
+            {
+                transform.position = new Vector3(transform.position.x, -23.97828f, transform.position.z);
+            }
+
+        }
+    }
+
+    void ReachedGoal()
+    {
+        Destroy(gameObject);
+    }
 }
