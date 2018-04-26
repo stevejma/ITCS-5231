d7a337bc66144db48ca3e5b65da7d7399fc74200
stevejma <35577054+stevejma@users.noreply.github.com>
2018-04-26 09:56:33 -0400

    Revert "OBJ"
    This reverts commit 2cfc29e045da61f5da155ae2fa5c0218427f21eb.

--- Brave New World/Assets/Scripts/CameraMovement.cs
+++ Brave New World/Assets/Scripts/CameraMovement.cs
@@ -26,33 +26,12 @@ public class CameraMovement : MonoBehaviour {
     public Camera Camera_05;
     public Camera currentCam;
 
+
+
     public Text infoText;
     public GameObject confirm;
 
-    public  string age;
-    public  string loyalty;
-    public  string gender;
-    public  string ethnicity;
-    public  string sexID;
-    public  string occupation;
-    public  string religion;
-    public  string maritalStatus;
-
-    public static string[] list = new string[8];
-
-    public Text missionText;
-
-    public AudioClip gameStartAudio;
-    public AudioSource source;
-
-    public static int mission;
-
     void Start() {
-        source.clip = gameStartAudio;
-        source.PlayDelayed(2);
-
-        CurrentMission();
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = true;
         confirm.SetActive(false);
@@ -61,11 +40,9 @@ public class CameraMovement : MonoBehaviour {
         Camera_03.enabled = false;
         Camera_04.enabled = false;
         Camera_05.enabled = false;
-
     }
 
     void Update() {
-        
 
         if (Input.GetKeyDown(KeyCode.Alpha1)) {
 
@@ -141,16 +118,6 @@ public class CameraMovement : MonoBehaviour {
             if (didHit) {
                 if (rhInfo.transform.gameObject.tag == "xNPC") {
                     Traits getTraits = rhInfo.transform.GetComponent<Traits>();
-
-                    list[0] = getTraits.Storing()[0];
-                    list[1] = getTraits.Storing()[1];
-                    list[2] = getTraits.Storing()[2];
-                    list[3] = getTraits.Storing()[3];
-                    list[4] = getTraits.Storing()[4];
-                    list[5] = getTraits.Storing()[5];
-                    list[6] = getTraits.Storing()[6];
-                    list[7] = getTraits.Storing()[7];
-
                     infoText.text = ("Target Info \nGender: " + getTraits.Storing()[0] +
                         "\nAge: " + getTraits.Storing()[1] +
                         "\nEthnicity: " + getTraits.Storing()[2] +
@@ -159,23 +126,13 @@ public class CameraMovement : MonoBehaviour {
                         "\nSexual Orientation: " + getTraits.Storing()[5] +
                         "\nParty Loyalty (1-10): " + getTraits.Storing()[6] +
                         "\nOccupation: " + getTraits.Storing()[7]);
-   
+
 
                     confirm.SetActive(true);
                     Debug.Log("Hit Female");
 
                 } else if (rhInfo.transform.gameObject.tag == "yNPC") {
                     Traits getTraits = rhInfo.transform.GetComponent<Traits>();
-
-                    list[0] = getTraits.Storing()[0];
-                    list[1] = getTraits.Storing()[1];
-                    list[2] = getTraits.Storing()[2];
-                    list[3] = getTraits.Storing()[3];
-                    list[4] = getTraits.Storing()[4];
-                    list[5] = getTraits.Storing()[5];
-                    list[6] = getTraits.Storing()[6];
-                    list[7] = getTraits.Storing()[7];
-
                     infoText.text = ("Target Info \nGender: " + getTraits.Storing()[0] +
                         "\nAge: " + getTraits.Storing()[1] +
                         "\nEthnicity: " + getTraits.Storing()[2] +
@@ -197,71 +154,5 @@ public class CameraMovement : MonoBehaviour {
                 confirm.SetActive(false);
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            PassToCompare(mission);
-        }
     }
-
-    void PassToCompare(int compare) {
-        if (compare == 2)
-        {
-            Mission2(list);
-        } else if (compare == 1)
-        {
-            Mission1(list);
-        } else
-        {
-            Mission0(list);
-        }
-    }
-
-    void CurrentMission() {
-        mission = Random.Range(0, 3);
-
-        switch(mission)
-        {
-            case 2:
-                missionText.text = "Mission Objectives: \nTarget is a political dissenter posting anti-government propaganda on the local state-sponsored university’s campus. Target may not necessarily be a student. Shows knowledge about university’s security systems. ";
-                break;
-            case 1:
-                missionText.text = "Mission Objectives: \nTarget is a frequent financial sponsor and local leader of Queer rights groups. Historically these groups have been marginally successful disrupting social order among youth culture by promoting literature against State-Cultural laws at privately run educational institutions.";
-                break;
-            case 0:
-                missionText.text = "Target is a local ring leader organizing musical venues that promote anti-establishment themes and values through various anonymous online postings. Shows proficiency in avoiding net censures.";
-                break;
-            default:
-                missionText.text = "Error";
-                break;
-        }
-
-    }
-
-    void Mission2(string [] list) {
-
-        if (list[7].Equals("Student") || list[7].Equals("Professor") || list[7].Equals("Network administrator")) {
-            Debug.Log("You win!");
-        } else { Debug.Log("You lose!"); }
-    }
-
-    void Mission1(string[] list)
-    {
-
-        if (list[5].Equals("Homosexual Tendencies") || list[5].Equals("Homosexual"))
-        {
-            Debug.Log("You win!");
-        }
-        else { Debug.Log("You lose!"); }
-    }
-    void Mission0(string[] list)
-    {
-
-        if (list[7].Equals("Musician") || list[7].Equals("Web content developer") || list[7].Equals("Network administrator"))
-        {
-            Debug.Log("You win!");
-        }
-        else { Debug.Log("You lose!"); }
-    }
-
 }
--- Brave New World/Assets/Scripts/MissionGenerator.cs
+++ Brave New World/Assets/Scripts/MissionGenerator.cs
@@ -3,18 +3,17 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MissionGenerator : MonoBehaviour
-{
+public class MissionGenerator : MonoBehaviour {
 
-    public static string objAge;
-    public static string objLyalty;
+    public int age;
+    public int loyalty;
 
-    public static string objGender;
-    public static string objEthnicity;
-    public static string objSexID;
-    public static string objOccupation;
-    public static string objReligion;
-    public static string obJmaritalStatus;
+    public string gender;
+    public string ethnicity;
+    public string sexID;
+    public string occupation;
+    public string religion;
+    public string maritalStatus;
 
     bool isIntroFinished = false;
 
@@ -24,8 +23,7 @@ public class MissionGenerator : MonoBehaviour
     public AudioSource source;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start () {
         source.clip = gameStartAudio;
         source.PlayDelayed(2);
 
@@ -33,18 +31,22 @@ public class MissionGenerator : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
-        if (source.time > 37)
-        {
-            CallMission();
-        }
+        if (source.time > 37) {
+            Mission1();
+        } 
 
     }
 
-    void CallMission()
-    {
+    void CallMission() {
+        
+    }
+   
+    void Mission1 () {
+        missionText.text = "Mission Objectives: \nTarget is a political dissenter posting anti-government propaganda on the local state-sponsored university’s campus. Target may not necessarily be a student. Shows knowledge about university’s security systems. ";
+
+        occupation = "Student";
 
     }
 }
